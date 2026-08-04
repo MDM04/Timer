e245afa791ddefc44c8fce1e4a9a77b882522036
MDM <mdm.pro4@gmail.com>
2026-08-03 22:25:26 -0300

    feat: add Hitory page

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,9 +1,109 @@
-export function History () {
-    return (
-        <div>
-            <h1>
-                History
-            </h1>
-        </div>
-    )
+import styled from "styled-components";
+
+const HistoryContainer = styled.main`
+  flex: 1;
+  padding: 3.5rem;
+  display: flex;
+  flex-direction: column;
+  h1 {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme["gray-100"]};
+  }
+`;
+
+const HistoryList = styled.div`
+  flex: 1;
+  overflow: auto;
+  margin-top: 2rem;
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    min-width: 600px;
+
+    th {
+      background-color: ${(props) => props.theme["gray-600"]};
+      padding: 1rem;
+      text-align: left;
+      color: ${(props) => props.theme["gray-100"]};
+      font-size: 0.875rem;
+      line-height: 1.6;
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
+      }
+    }
+
+    td {
+      background-color: ${(props) => props.theme["gray-700"]};
+      border-top: 4px solid ${(props) => props.theme["gray-800"]};
+      padding: 1rem;
+      font-size: 0.875rem;
+      line-height: 1.6;
+      &:first-child {
+        width: 50%;
+        padding-left: 1.5rem;
+      }
+      &:last-child {
+        padding-right: 1.5rem;
+      }
+    }
+  }
+`;
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Tarefa </td>
+              <td>20 minutos </td>
+              <td>Há 2 meses </td>
+              <td>Concluido </td>
+            </tr>
+            <tr>
+              <td>Tarefa </td>
+              <td>20 minutos </td>
+              <td>Há 2 meses </td>
+              <td>Concluido </td>
+            </tr>
+            <tr>
+              <td>Tarefa </td>
+              <td>20 minutos </td>
+              <td>Há 2 meses </td>
+              <td>Concluido </td>
+            </tr>
+            <tr>
+              <td>Tarefa </td>
+              <td>20 minutos </td>
+              <td>Há 2 meses </td>
+              <td>Concluido </td>
+            </tr>
+            <tr>
+              <td>Tarefa </td>
+              <td>20 minutos </td>
+              <td>Há 2 meses </td>
+              <td>Concluido </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
 }
