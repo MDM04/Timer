f16a7434601598c2e2b9aa19f56e622b4c32d432
MDM <mdm.pro4@gmail.com>
2026-08-11 22:22:30 -0300

    feat: Add ts Adding TypeScript to the form

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -119,12 +119,20 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, "O ciclo precisa ser de no máximo 60 minutos."),
 });
 
+
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
   
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    }
   });
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data);
   }
 
